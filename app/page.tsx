22ca9ea6790fe148e3d6989db7abb061e31ae1a6
Suat Bayrak <suat.bayrak@bilgiedu.net>
2025-10-14 17:35:12 +0200

    add svg bg img
    add eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Home() { 
   return (
-    <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-      <span>some text</span>
+    <div>
+      <span className='text-white'>some text</span>
     </div>
   );
 }
