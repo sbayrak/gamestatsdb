cd3ae6894fadbef86e97f0d8be2147fbde60bab0
Suat Bayrak <suat.bayrak@bilgiedu.net>
2025-10-14 21:31:09 +0200

    fix eslint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
   title: 'GamestatsDB | Steam Game Stats Database',
@@ -16,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang='en'>
