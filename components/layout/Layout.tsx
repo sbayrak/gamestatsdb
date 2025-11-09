a0f1b6eaddc9e43e5fba93b4839f9459cd827e0c
Suat Bayrak <suat.bayrak@bilgiedu.net>
2025-11-09 20:08:24 +0100

    add layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -7,12 +7,7 @@ interface LayoutProps extends PropsWithChildren {
 
 export default function Layout({ className, children }: LayoutProps) {
   return (
-    <div
-      className={cn(
-        'bg-transparent relative h-96 max-w-5xl border-2 border-blue-500 border-solid mx-auto',
-        className
-      )}
-    >
+    <div className={cn('bg-transparent relative max-w-5xl mx-auto', className)}>
       {children}
     </div>
   );
