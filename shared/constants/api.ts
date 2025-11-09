3f617ea857d75034a4a7be7ab105c48fc9edc154
Suat Bayrak <suat.bayrak@bilgiedu.net>
2025-11-09 20:01:51 +0100

    export constants

---
+++ shared/constants/api.ts
@@ -0,0 +1,6 @@
+export const STEAM_MOST_PLAYED_API =
+  'https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/';
+export const STEAM_APP_DETAILS_API =
+  'https://store.steampowered.com/api/appdetails?appids=';
+
+export const TRENDING_API_ROUTE = '/api/trending';
