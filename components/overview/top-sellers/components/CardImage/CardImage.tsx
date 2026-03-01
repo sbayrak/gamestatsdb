0aeefa8a90ed549f8a919f63c3135c7dc59e253e
Suat Bayrak <suat.bayrak@bilgiedu.net>
2026-03-01 15:09:51 +0100

    have fallback img

--- components/overview/top-sellers/components/CardImage/CardImage.tsx
+++ components/overview/top-sellers/components/CardImage/CardImage.tsx
@@ -1,15 +1,21 @@
+"use client";
+
 import Image from "next/image";
 
+const FALLBACK_IMAGE = "/no-image.svg";
+
 interface CardImageProps {
-  src: string;
+  src?: string;
   alt: string;
 }
 
 export default function CardImage({ alt, src }: CardImageProps) {
+  const imgSrc = src || FALLBACK_IMAGE;
+
   return (
     <div className="relative w-full">
       <Image
-        src={src}
+        src={imgSrc}
         alt={alt}
         width={0}
         height={0}
